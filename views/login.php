eb733560c3677a0423d78862591602c2b6d1237b
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2026-01-17 14:48:14 +0600

    feat: enhance login form with error handling and input retention

--- views/login.php
+++ views/login.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+$errors = $_SESSION['errors'] ?? [];
+$oldInput = $_SESSION['old_input'] ?? [];
+
+$emailErr = $errors['emailErr'] ?? '';
+$passwordErr = $errors['passwordErr'] ?? '';
+
+unset($_SESSION['errors']);
+unset($_SESSION['old_input']);
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,12 +17,18 @@
     </head>
     <body>
         <div class="form-box">
-<form class="form">
+<form class="form" method="post" action="../controllers/authControl.php">
     <span class="title">Log in</span>
     <span class="subtitle">Log in with your email and password.</span>
     <div class="form-container">
-		<input type="email" class="input" placeholder="Email">
-		<input type="password" class="input" placeholder="Password">
+		<input type="email" class="input" name="email" placeholder="Email" value="<?php echo htmlspecialchars($oldInput['email'] ?? ''); ?>">
+        <?php if ($emailErr): ?>
+            <span style="color: red; font-size: 12px;"><?php echo $emailErr; ?></span>
+        <?php endif; ?>
+		<input type="password" class="input" name="pass" placeholder="Password">
+        <?php if ($passwordErr): ?>
+            <span style="color: red; font-size: 12px;"><?php echo $passwordErr; ?></span>
+        <?php endif; ?>
     </div>
     <button>Log in</button>
 </form>
