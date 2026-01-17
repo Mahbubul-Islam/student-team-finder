712e199471cf0d9b79373122648c67c58206eb07
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2026-01-17 13:48:45 +0600

    feat: add user retrieval and creation functions with database integration

---
+++ models/users.php
@@ -0,0 +1,72 @@
+<?php
+    require_once("dbConnect.php");
+
+    function getUserByEmail($email) {
+        $conn = dbConnect();
+        $query = "SELECT * FROM users WHERE email = ?";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $data = $stmt->get_result();
+        if ($data->num_rows > 0) {
+            $user = $data->fetch_assoc();
+        } else {
+            $user = null;
+        }
+        $stmt->close();
+        $conn->close();
+        return $user;
+    }
+
+    function createUser($name, $email, $hashedPassword, $role, $gender) {
+        $conn = dbConnect();
+        $query = "INSERT INTO users (name, email, password, role, gender) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("sssss", $name, $email, $hashedPassword, $role, $gender);
+        $stmt->execute();
+        // $userRole 
+        $stmt->close();
+        $conn->close();
+        // return $userRole;
+    }
+
+    // function getRoleByEmail($email) {
+    //     $conn = dbConnect();
+    //     $query = "SELECT * FROM users WHERE email = ?";
+
+    //     $stmt = $conn->prepare($query);
+    //     $stmt->bind_param("s", $email);
+    //     $stmt->execute();
+    //     $data = $stmt->get_result();
+    //     if ($data->num_rows > 0) {
+    //         $user = $data->fetch_assoc();
+    //     } else {
+    //         $user = null;
+    //     }
+    //     $stmt->close();
+    //     $conn->close();
+    //     return $user;
+    // }
+
+    // fucntion authUser($email, $hashedPassword){
+    //     $conn = dbConnect();
+    //         $query = "SELECT * FROM users WHERE email = ? AND password = ?";
+    //         $stmt = $conn->prepare($query);
+    //         $stmt->bind_param("ss", $email, $hashedPassword);
+    //         $stmt->execute();
+    //         $data = $stmt->get_result();
+    //         if ($data->num_rows > 0) {
+    //             $user = $data->fetch_assoc();
+    //         } else {
+    //             $user = null;
+    //         }
+    //         $stmt->close();
+    //         $conn->close();
+    //         return $user;
+
+    // }
+
+
+
+?>
